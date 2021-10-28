db0fd2c696bd1ccfb30ab1d9165ca44ae74bfaa4
jdcera4 <jdcera4@gmail.com>
2021-10-28 15:02:00 -0500

    create layout

--- admin/src/app/app-routing.module.ts
+++ admin/src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ParksComponent } from './parks/parks.component';
 
-const routes: Routes = [{path: 'parks', component: ParksComponent}];
+
+const routes: Routes = [
+                        { path: '', redirectTo: '/parks', pathMatch: 'full' },
+                        {path: 'parks', component: ParksComponent}
+                      ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ParksComponent } from './parks/parks.component';
+import { LayoutComponent } from './layout/layout.component';
+
+import { SidebarModule } from 'ng-sidebar';
 
 @NgModule({
-  declarations: [AppComponent, ParksComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent, 
+    ParksComponent, LayoutComponent
+  ],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule,
+    SidebarModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
