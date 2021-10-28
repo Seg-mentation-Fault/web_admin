2f65edfbd3307902abb0f3996b1906e37f6292d4
Felipe Prado <jfpc11@misena.edu.co>
2021-10-28 08:00:06 -0500

    creating a layout

--- admin/src/app/app-routing.module.ts
+++ admin/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ParksComponent } from './parks/parks.component';
+import { ReservationComponent } from './reservation/reservation.component';
 
-const routes: Routes = [{path: 'parks', component: ParksComponent}];
+const routes: Routes = [
+  { path: '', redirectTo: '/parks', pathMatch: 'full' },
+  { path: 'parks', component: ParksComponent },
+  { path: 'reservations', component: ReservationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SidebarModule } from 'ng-sidebar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ParksComponent } from './parks/parks.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, ParksComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, ParksComponent, ReservationComponent, NavBarComponent],
+  imports: [BrowserModule, AppRoutingModule, SidebarModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
